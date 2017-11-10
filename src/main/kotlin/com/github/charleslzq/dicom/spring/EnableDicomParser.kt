f36eb7e30a181027cfb72650bb4034f1cc182db4
Eric Charles <github@ericcharleslzq.com>
2017-11-10 14:00:10 +0800

    add enable annotation

---
+++ src/main/kotlin/com/github/charleslzq/dicom/spring/EnableDicomParser.kt
@@ -0,0 +1,12 @@
+package com.github.charleslzq.dicom.spring
+
+import org.springframework.context.annotation.Import
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE, AnnotationTarget.ANNOTATION_CLASS)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Import(
+        DicomStoreConfiguration::class,
+        DicomDataReaderConfiguration::class,
+        DicomFileWatchConfiguration::class)
+annotation class EnableDicomParser
